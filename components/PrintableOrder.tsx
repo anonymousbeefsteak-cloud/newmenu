89fe637909475d0ea73f5adaff4a392eaaf9d09a
anonymousbeefsteak-cloud <anonymousbeefsteak@gmail.com>
2025-11-08 12:25:06 +0800

    Update PrintableOrder.tsx

--- components/PrintableOrder.tsx
+++ components/PrintableOrder.tsx
@@ -44,17 +44,37 @@ export const PrintableOrder: React.FC<PrintableOrderProps> = ({ order, orderId }
             style.innerHTML = printStyle;
             document.head.appendChild(style);
             
-            // 延遲後執行列印
-            setTimeout(() => {
-                window.print();
-                
-                // 列印完成後重新載入
-                window.onafterprint = function() {
-                    setTimeout(() => {
-                        window.location.reload();
-                    }, 1000);
+            // 使用 Promise 來處理列印
+            const printPromise = new Promise((resolve) => {
+                // 監聽列印完成事件
+                const handleAfterPrint = () => {
+                    resolve(true);
                 };
-            }, 500);
+                
+                // 添加事件監聽
+                window.addEventListener('afterprint', handleAfterPrint);
+                
+                // 開始列印
+                setTimeout(() => {
+                    window.print();
+                }, 500);
+                
+                // 備用方案：如果 afterprint 事件沒觸發，5秒後強制重新載入
+                setTimeout(() => {
+                    resolve(true);
+                }, 5000);
+            });
+            
+            // 列印完成後重新載入
+            printPromise.then(() => {
+                // 移除列印樣式
+                document.head.removeChild(style);
+                
+                // 重新載入頁面
+                setTimeout(() => {
+                    window.location.href = window.location.origin + window.location.pathname;
+                }, 1000);
+            });
         };
         
         printAutomatically();
@@ -70,184 +90,99 @@ export const PrintableOrder: React.FC<PrintableOrderProps> = ({ order, orderId }
             fontWeight: 'bold',
             fontFamily: 'Arial, sans-serif'
         }}>
-            {/* 第一份資料 */}
-            <div style={{ marginBottom: '30px' }}>
-                {/* 單號和餐點內容標題 */}
-                <div style={{ marginBottom: '15px', textAlign: 'center' }}>
-                    單號: {finalOrderId} 餐點內容 -
-                </div>
+            {/* 單號和餐點內容標題 */}
+            <div style={{ marginBottom: '15px', textAlign: 'center' }}>
+                單號: {finalOrderId} 餐點內容 -
+            </div>
 
-                {/* 餐點總計 */}
+            {/* 餐點總計 */}
+            <div style={{ 
+                marginBottom: '15px', 
+                textAlign: 'center'
+            }}>
+                餐點總計: ${2237}
+            </div>
+
+            {/* 小計標題 */}
+            <div style={{ 
+                marginBottom: '15px', 
+                textAlign: 'center'
+            }}>
+                小計
+            </div>
+
+            {/* 餐點項目 */}
+            <div style={{ 
+                marginBottom: '15px'
+            }}>
+                <div style={{ marginBottom: '10px' }}>
+                    板腱牛排+脆皮炸雞(炸魚)套餐 x3($1737)
+                </div>
                 <div style={{ 
-                    marginBottom: '15px', 
+                    marginBottom: '10px',
                     textAlign: 'center'
                 }}>
-                    餐點總計: ${2237}
+                    數量
                 </div>
-
-                {/* 小計標題 */}
-                <div style={{ 
-                    marginBottom: '15px', 
-                    textAlign: 'center'
-                }}>
-                    小計
-                </div>
-
-                {/* 餐點項目 */}
-                <div style={{ 
-                    marginBottom: '15px'
-                }}>
-                    <div style={{ marginBottom: '10px' }}>
-                        板腱牛排+脆皮炸雞(炸魚)套餐 x3($1737)
-                    </div>
-                    <div style={{ 
-                        marginBottom: '10px',
-                        textAlign: 'center'
-                    }}>
-                        數量
-                    </div>
-                    <div>
-                        英式炸魚套餐 x2($500)
-                    </div>
-                </div>
-
-                {/* 分隔線 */}
-                <div style={{ 
-                    margin: '15px 0', 
-                    textAlign: 'center',
-                    borderTop: '2px solid black',
-                    paddingTop: '10px'
-                }}>
-                    - 總計列表 -
-                </div>
-
-                {/* 炸物總計 */}
-                <div style={{ marginBottom: '12px' }}>
-                    <div style={{ marginBottom: '6px' }}>炸物總計:</div>
-                    <div style={{ paddingLeft: '20px' }}>
-                        <div>- 炸魚 x3</div>
-                    </div>
-                </div>
-
-                {/* 飲料總計 */}
-                <div style={{ marginBottom: '12px' }}>
-                    <div style={{ marginBottom: '6px' }}>飲料總計:</div>
-                    <div style={{ paddingLeft: '20px' }}>
-                        <div>- 無糖紅茶 x3</div>
-                        <div>- 冰涼可樂 x2</div>
-                    </div>
-                </div>
-
-                {/* 醬料總計 */}
-                <div style={{ marginBottom: '12px' }}>
-                    <div style={{ marginBottom: '6px' }}>醬料總計:</div>
-                    <div style={{ paddingLeft: '20px' }}>
-                        <div>- 泡菜 x4</div>
-                        <div>- 生蒜片 x2</div>
-                        <div>- 黑胡椒 x2</div>
-                        <div>- 巴薩米克醋 x1</div>
-                        <div>- 蒜味醬 x1</div>
-                    </div>
-                </div>
-
-                {/* 加購總計 */}
-                <div style={{ marginBottom: '12px' }}>
-                    <div style={{ marginBottom: '6px' }}>加購總計:</div>
-                    <div style={{ paddingLeft: '20px' }}>
-                        <div>- 豬排加購 5oz x2</div>
-                    </div>
+                <div>
+                    英式炸魚套餐 x2($500)
                 </div>
             </div>
 
-            {/* 第二份資料（重複第一份） */}
+            {/* 分隔線 */}
             <div style={{ 
-                borderTop: '3px dashed #666',
-                paddingTop: '20px'
+                margin: '15px 0', 
+                textAlign: 'center',
+                borderTop: '2px solid black',
+                paddingTop: '10px'
             }}>
-                {/* 單號和餐點內容標題 */}
-                <div style={{ marginBottom: '15px', textAlign: 'center' }}>
-                    單號: {finalOrderId} 餐點內容 -
-                </div>
+                - 總計列表 -
+            </div>
 
-                {/* 餐點總計 */}
-                <div style={{ 
-                    marginBottom: '15px', 
-                    textAlign: 'center'
-                }}>
-                    餐點總計: ${2237}
+            {/* 炸物總計 */}
+            <div style={{ marginBottom: '12px' }}>
+                <div style={{ marginBottom: '6px' }}>炸物總計:</div>
+                <div style={{ paddingLeft: '20px' }}>
+                    <div>- 炸魚 x3</div>
                 </div>
+            </div>
 
-                {/* 小計標題 */}
-                <div style={{ 
-                    marginBottom: '15px', 
-                    textAlign: 'center'
-                }}>
-                    小計
+            {/* 飲料總計 */}
+            <div style={{ marginBottom: '12px' }}>
+                <div style={{ marginBottom: '6px' }}>飲料總計:</div>
+                <div style={{ paddingLeft: '20px' }}>
+                    <div>- 無糖紅茶 x3</div>
+                    <div>- 冰涼可樂 x2</div>
                 </div>
+            </div>
 
-                {/* 餐點項目 */}
-                <div style={{ 
-                    marginBottom: '15px'
-                }}>
-                    <div style={{ marginBottom: '10px' }}>
-                        板腱牛排+脆皮炸雞(炸魚)套餐 x3($1737)
-                    </div>
-                    <div style={{ 
-                        marginBottom: '10px',
-                        textAlign: 'center'
-                    }}>
-                        數量
-                    </div>
-                    <div>
-                        英式炸魚套餐 x2($500)
-                    </div>
+            {/* 醬料總計 */}
+            <div style={{ marginBottom: '12px' }}>
+                <div style={{ marginBottom: '6px' }}>醬料總計:</div>
+                <div style={{ paddingLeft: '20px' }}>
+                    <div>- 泡菜 x4</div>
+                    <div>- 生蒜片 x2</div>
+                    <div>- 黑胡椒 x2</div>
+                    <div>- 巴薩米克醋 x1</div>
+                    <div>- 蒜味醬 x1</div>
                 </div>
+            </div>
 
-                {/* 分隔線 */}
-                <div style={{ 
-                    margin: '15px 0', 
-                    textAlign: 'center',
-                    borderTop: '2px solid black',
-                    paddingTop: '10px'
-                }}>
-                    - 總計列表 -
+            {/* 牛肉熟度總計 */}
+            <div style={{ marginBottom: '12px' }}>
+                <div style={{ marginBottom: '6px' }}>牛肉熟度總計:</div>
+                <div style={{ paddingLeft: '20px' }}>
+                    <div>- 3分熟 x2</div>
+                    <div>- 5分熟 x3</div>
+                    <div>- 7分熟 x1</div>
                 </div>
+            </div>
 
-                {/* 炸物總計 */}
-                <div style={{ marginBottom: '12px' }}>
-                    <div style={{ marginBottom: '6px' }}>炸物總計:</div>
-                    <div style={{ paddingLeft: '20px' }}>
-                        <div>- 炸魚 x3</div>
-                    </div>
-                </div>
-
-                {/* 飲料總計 */}
-                <div style={{ marginBottom: '12px' }}>
-                    <div style={{ marginBottom: '6px' }}>飲料總計:</div>
-                    <div style={{ paddingLeft: '20px' }}>
-                        <div>- 無糖紅茶 x3</div>
-                        <div>- 冰涼可樂 x2</div>
-                    </div>
-                </div>
-
-                {/* 醬料總計 */}
-                <div style={{ marginBottom: '12px' }}>
-                    <div style={{ marginBottom: '6px' }}>醬料總計:</div>
-                    <div style={{ paddingLeft: '20px' }}>
-                        <div>- 泡菜 x4</div>
-                        <div>- 生蒜片 x2</div>
-                        <div>- 黑胡椒 x2</div>
-                        <div>- 巴薩米克醋 x1</div>
-                        <div>- 蒜味醬 x1</div>
-                    </div>
-                </div>
-
-                {/* 加購總計 */}
-                <div style={{ marginBottom: '12px' }}>
-                    <div style={{ marginBottom: '6px' }}>加購總計:</div>
-                    <div style={{ paddingLeft: '20px' }}>
-                        <div>- 豬排加購 5oz x2</div>
-                    </div>
+            {/* 加購總計 */}
+            <div style={{ marginBottom: '12px' }}>
+                <div style={{ marginBottom: '6px' }}>加購總計:</div>
+                <div style={{ paddingLeft: '20px' }}>
+                    <div>- 豬排加購 5oz x2</div>
                 </div>
             </div>
         </div>
